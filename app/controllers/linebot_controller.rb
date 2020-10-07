802a10d9e1fe93dfbc0e3b209b0fced11f0dda4c
Daisuke-23 <md-b8c6.p16w@i.softbank.jp>
2020-10-07 18:16:20 +0900

    修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -4,7 +4,6 @@ class LinebotController < ApplicationController
   require 'kconv'
   require 'rexml/document'
 
-  # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
   def callback
@@ -16,23 +15,17 @@ class LinebotController < ApplicationController
     events = client.parse_events_from(body)
     events.each { |event|
       case event
-        # メッセージが送信された場合の対応（機能①）
       when Line::Bot::Event::Message
         case event.type
-          # ユーザーからテキスト形式のメッセージが送られて来た場合
         when Line::Bot::Event::MessageType::Text
-          # event.message['text']：ユーザーから送られたメッセージ
           input = event.message['text']
           url  = "https://www.drk7.jp/weather/xml/13.xml"
           xml  = open( url ).read.toutf8
           doc = REXML::Document.new(xml)
           xpath = 'weatherforecast/pref/area[4]/'
-          # 当日朝のメッセージの送信の下限値は20％としているが、明日・明後日雨が降るかどうかの下限値は30％としている
           min_per = 30
           case input
-            # 「明日」or「あした」というワードが含まれる場合
           when /.*(明日|あした).*/
-            # info[2]：明日の天気
             per06to12 = doc.elements[xpath + 'info[2]/rainfallchance/period[2]'].text
             per12to18 = doc.elements[xpath + 'info[2]/rainfallchance/period[3]'].text
             per18to24 = doc.elements[xpath + 'info[2]/rainfallchance/period[4]'].text
@@ -81,7 +74,7 @@ class LinebotController < ApplicationController
                 "今日の天気？\n今日は雨は降らなさそうだよ。\n#{word}"
             end
           end
-          # テキスト以外（画像等）のメッセージが送られた場合
+          
         else
           push = "テキスト以外はわからないよ〜(；；)"
         end
@@ -90,14 +83,14 @@ class LinebotController < ApplicationController
           text: push
         }
         client.reply_message(event['replyToken'], message)
-        # LINEお友達追された場合（機能②）
+        
       when Line::Bot::Event::Follow
-        # 登録したユーザーのidをユーザーテーブルに格納
+        
         line_id = event['source']['userId']
         User.create(line_id: line_id)
-        # LINEお友達解除された場合（機能③）
+        
       when Line::Bot::Event::Unfollow
-        # お友達解除したユーザーのデータをユーザーテーブルから削除
+       
         line_id = event['source']['userId']
         User.find_by(line_id: line_id).destroy
       end
